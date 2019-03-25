99d7e307ba2950843c73350879e7e588dbfcd700
Александр <grishkevichaaa@gmail.com>
2019-03-25 19:40:38 +0300

    Add employee page

---
+++ USP/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using USP.Models;
+
+namespace USP.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [Authorize]
+        public IActionResult Index(int id, EmployeeContext employeeContext)
+        {
+            Employee employee = employeeContext.GetEmployee(id);
+            return PartialView(employee);
+        }
+    }
+}
